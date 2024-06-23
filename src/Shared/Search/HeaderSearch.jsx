bdfebd9ff3bbe7e6d150f25ae83b83f4e41024bf
jisan5620jk <jisank5620@gmail.com>
2024-06-23 09:58:31 +0600

    Update Componant

--- src/Shared/Search/HeaderSearch.jsx
+++ src/Shared/Search/HeaderSearch.jsx
@@ -1,27 +1,23 @@
 import { BiSearch } from "react-icons/bi";
 import "./header-search.css";
 import { FaArrowUp } from "react-icons/fa6";
-import { SlClose } from "react-icons/sl";
 
 const Search = () => {
   if (document.querySelector(".search-box-outer")) {
     document
       .querySelector(".search-box-outer")
-      .addEventListener("click", function () {
+      .addEventListener("click", () => {
         document.body.classList.add("search-active");
       });
     document
       .querySelector(".close-search")
-      .addEventListener("click", function () {
+      .addEventListener("click", () => {
         document.body.classList.remove("search-active");
       });
   }
 
   return (
     <div className="search-popup">
-      <button className="close-search style-two">
-        <SlClose />
-      </button>
       <button className="close-search">
         <FaArrowUp />
       </button>
@@ -30,7 +26,7 @@ const Search = () => {
           <input
             type="search"
             name="search"
-            placeholder="Search Here"
+            placeholder="Search here..."
             required
           />
           <button type="submit">
